84a32fb2b534d983fc166e54442e919a27195b8d
Poing3000 <85301862+Poing3000@users.noreply.github.com>
2023-06-18 19:37:53 +0100

    init

---
+++ FP3000_PicoW/FP3000_PicoW.ino
@@ -0,0 +1,62 @@
+/*
+ * Name:	Futterpumpe / FoodPump3000
+ * Author:	Poing3000
+ * Status:	Dev
+ *
+ * Description:
+ * This is the code for the cat food pump, called Futterpumpe (Foodpump3000). 
+ * It is built for a Raspberry Pico W and a TMC2209 stepper driver.
+ * Further info at: https://github.com/Poing3000/FoodPump3000
+ * 
+ * Updates - DEV:
+ * [ ] - Prime (find endstop)
+ * [ ] - Approx. filling
+ * [ ] - Accurate filling
+ * [ ] - TMC2209 Prime with IR Sensor check
+ * [ ] - Calibrate with IR Sensor
+ * [ ] - Error Management:
+ *    [ ] - Detect Calibration Error (dif. IR vs Stall)
+ *    [ ] - Handle unexpected Stall
+ *    [ ] - EMGY Mode
+ * [ ] - Comunicate
+ *
+ */
+
+
+//Parts for individual configuration are highlited below.
+//----------------------------------------------------------
+//CONFIGURATION:
+    //Libraries
+    //---------------------------------
+        //External
+        //#include ExLib...
+    //---------------------------------*
+
+    //General Config
+    //---------------------------------
+
+    //---------------------------------*
+
+//END OF CONFIG+++++++++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//FUNCTIONS:
+    //Stall indication
+
+//END OF FUNCTIONS++++++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//SETUP:
+void setup() {
+
+}
+//END OF SETUP++++++++++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//MAIN PROGRAM (LOOP):
+void loop() {
+    // put your main code here, to run repeatedly:
+
+}
+//END OF MAIN PROGRAM+++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
