ba5481b905738c550fe11b9a79c0b4f4c29e012a
Poing3000 <85301862+Poing3000@users.noreply.github.com>
2024-01-18 18:38:12 +0000

    Use of FP3000 library

---
+++ FP3000_PicoW/FP3000.h
@@ -0,0 +1,45 @@
+/*
+ * Name:	FoodPump3000
+ * Author:	Poing3000
+ * Status:	Dev
+ *
+ * Description:
+ * This code is header for the cat food pump, also called Futterpumpe or Foodpump3000.
+ * It is ment to be a libary used for Arduino compatible microcontrollers.
+ * Further info at: https://github.com/Poing3000/FoodPump3000
+*/
+
+#ifndef _FP3000_h
+#define _FP3000_h
+
+#include <Arduino.h>
+#include <SpeedyStepper4Purr.h>
+#include <TMCStepper.h>
+
+
+class FP3000 {
+
+public:
+
+	// pulblic members
+	FP3000(byte MotorNumber, long std_distance, long dir_home, HardwareSerial& serial, float driver_rsense, uint8_t driver_adress);
+	
+	void SetupPump(uint16_t motor_current, uint8_t stall_val, uint16_t mic_steps, uint32_t tcool,
+		byte step_pin, byte dir_pin, byte limit_pin, byte diag_pin, float stepper_speed, float stepper_accel, long max_range);
+
+	//void PumpFood(byte food_amount);
+	void Test(bool moveUP);
+
+private:
+
+	// private members
+	SpeedyStepper4Purr StepperMotor;
+	TMC2209Stepper StepperDriver;
+
+	//byte _PumpNumber;						// NEEDED / DELETE?
+	long _std_distance;						// Standard range (steps) the slider should moves when feeding
+	long _dir_home;							// Direction to home (1 = CW, -1 = CCW)
+
+};
+
+#endif
--- FP3000_PicoW/FP3000_PicoW.ino
+++ FP3000_PicoW/FP3000_PicoW.ino
@@ -26,6 +26,7 @@
  * [ ] - Comunicate
  * [ ] - Handle/implement multicore. >> Comunication on Core 0, Motor on Core 1.
  * [ ] - Implement multible pumps control.
+ * [ ] - Make libraies local (past in repository and change from #include <LibraryFile.h>  to "LocalFile.h")
  * 
  * [ ] - *Future wish: Use RP2040 PIO for Stepper control. 
  * 
@@ -59,63 +60,92 @@
  // ----------------------------------------------------------
  // Libraries
 	// Stepper
-	#include <TMCStepper.h>
-	#include <SpeedyStepper4Purr.h>
+	#include "FP3000.h"
+	#include <TMCStepper.h> //DELETE LATER
+	#include <SpeedyStepper4Purr.h> //DELETE LATER
 
 	// Debugging
 	#include <Arduino_DebugUtils.h>
+
+	// Local Functions
+	// [...]
 // ---------------------------------*
 
-// DELETE LATER!!! INDICATOR LED
-#define LED  7
 
-// Global (NEEDS TO BE ADAPTED/CHANGED FOR LIBRARY!!!)
-#define ENABLE          15      // Enable Pin
-#define	SERIAL_PORT_2	Serial2	// HardwareSerial port (TX: 8, RX: 9)
-#define R_SENSE         0.11f   // Sense resistor value of the driver fur current cal.
-#define CURRENT         200    // Max current (mA) supplied to the motor
-#define MIRCO_STEPS     32      // Set microsteps (32 is a good compromise between CPU load and noise)
-#define TCOOLS          400		// max 20 bits
-#define MAX_RANGE       6600    // Max physical slider range is app. 6230 steps. Thus, 6600 is a safe value for error management.
-#define	STALL_VALUE		50		// Stall threshold [0..255] (lower = more sensitive) - 30 is quite low, yet safe for stable error management (50 for accuracy)
-#define START_DIST_S	200		// Start distance to go from endstop (only used for stall homing)
+// Variables CORE 0
+	//TODO: CHECK IF NEEDED / CAN BE USED LOCALY
+	const int Feed_Range_c0 = 4600;	// Typical feeding distance
 
-// Stepper Driver (TMC2209)
-#define DIR_1	11				// Direction pin
-#define STEP_1	12				// Step pin
-#define	DIAG_1	13				// DIAG pin for stall detection
-#define	DRIVER_ADDRESS_1 0b00	// Drivers address
+	// Debugging
+	#define DEBUG_LEVEL DBG_VERBOSE	//Set desired debug level
+		/* ---------------------------------
+		DBG_NONE - no debug output is shown
+		DBG_ERROR - critical errors
+		DBG_WARNING - non - critical errors
+		DBG_INFO - information
+		DBG_DEBUG - more information
+		DBG_VERBOSE - most information
+		Further info at: https://github.com/arduino-libraries/Arduino_DebugUtils
+		------------------------------------
+		*/
+
+//END CORE 0 ---------------------------------*
+
+// Variables CORE 1
+	//TODO: CHECK IF NEEDED / CAN BE USED LOCALY
+	byte Mode_c1	= 0;			// Mode Core 1 (Pumps); start with IDLE
+	byte Power_c1	= true;			// Indication if motors and secondaries are powered
+
+	// Stepper Settings
+ 
+		// General Stepper Driver Settings (change according to your connections to the stepper driver / your needs).
+		// Library settings (TMCStepper.h) can be foound at: https://teemuatlut.github.io/TMCStepper/class_t_m_c2209_stepper.html
+			// Driver (here default for TMC2209)
+			#define ENABLE				15			// Enable Pin
+			#define	SERIAL_PORT_2		Serial2		// HardwareSerial port (TX: 8, RX: 9)
+			#define R_SENSE				0.11f		// Sense resistor value of the driver fur current cal.
+			#define	DRIVER_ADDRESS_1	0b00		// Drivers address
+
+			#define CURRENT				400			// Max current (mA) supplied to the motor
+			#define	STALL_VALUE			50			// Stall threshold [0..255] (lower = more sensitive) - 30 is quite low, yet safe for stable error management (50 for accuracy)
+			#define MIRCO_STEPS			32			// Set microsteps (32 is a good compromise between CPU load and noise)
+			#define TCOOLS				400			// max 20 bits
+
+			// Stepper Motor (NEMA 17)
+			#define SPEED				10000		// Speed (steps/s) (10000 is good)
+			#define ACCEL				100000		// Acceleration (steps/s^2) (100000	is good)
+			#define STD_FEED_DIST		4600		// Standard range (steps) the slider should moves when feeding (4600 is good)
+			#define	PUMP_MAX_RANGE		6000		// Max range (steps) the slider can move inside the pump (6000 is good)
+
+		// Unique Motor Settings (change according to your connections to the stepper driver)
+			// Stepper Motor 0
+			#define MOTOR_0				0			// Uniuqe stepper motor number
+			#define	STEP_0				12			// Step pin
+			#define	DIR_0				11			// Direction pin
+			#define	LIMIT_0				4			// Limit switch pin
+			#define	DIAG_0				13			// DIAG pin for stall detection
+			#define DIR_TO_HOME			-1			// Direction to home (1 = CW, -1 = CCW)
+			
+
+
 //---------------------------------*
 
-// Stepper Motor (NEMA 17)
-#define LIMIT_1	4				// Limit switch pin
-#define SPEED	10000			// Speed (steps/s) (10000 is good)
-#define ACCEL	100000			// Acceleration (steps/s^2) (100000	is good)
 
-TMC2209Stepper driver_1(&SERIAL_PORT_2, R_SENSE, DRIVER_ADDRESS_1);	// Create Driver
-//SpeedyStepper4Purr stepper_0 (0);									// Create Stepper motor	
-SpeedyStepper4Purr stepper_1 (1);
-//---------------------------------*
 
-//Variables
-const int Feed_Range = 4600;	// Typical feeding distance
+// TESTING - DELETE LATER
+uint32_t  FIFO_c0 = 0;			// FIFO message from Core 0 to Core 1
+uint32_t  FIFO_c1 = 0;			// FIFO message from Core 1 to Core 0
+uint32_t  FIFO_R_c0 = 0;		// FIFO message read from at Core 0
+uint32_t  FIFO_R_c1 = 0;		// FIFO message read from at Core 1
 
 // Flags
 //[...]
 //---------------------------------*
 
-// Debugging
-#define DEBUG_LEVEL DBG_VERBOSE	//Set desired debug level
-	/* ---------------------------------
-	DBG_NONE - no debug output is shown
-	DBG_ERROR - critical errors
-	DBG_WARNING - non - critical errors
-	DBG_INFO - information
-	DBG_DEBUG - more information
-	DBG_VERBOSE - most information
-	Further info at: https://github.com/arduino-libraries/Arduino_DebugUtils
-	------------------------------------
-	*/
+// Create Pumps
+// Add pumps here if needed (e.g. 
+FP3000 Pump_0(MOTOR_0, STD_FEED_DIST, DIR_TO_HOME, SERIAL_PORT_2, R_SENSE, DRIVER_ADDRESS_1);
+//---------------------------------*
 
 
 // END OF CONFIG+++++++++++++++++++++++++++++++++++++++++++++
@@ -127,61 +157,134 @@ const int Feed_Range = 4600;	// Typical feeding distance
 // END OF FUNCTIONS++++++++++++++++++++++++++++++++++++++++++
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// SETUP:
+// SETUP - CORE 0:
 void setup() {
+
+	rp2040.idleOtherCore(); // Stop Core 1
 
 	//DELETE DELAY FOR SERIAL
 	delay(2000);
 
 	// Debugging
 	Debug.setDebugLevel(DEBUG_LEVEL);
-		// DELETE LATER! DEBUG/TESTING
-		pinMode(LED, OUTPUT);				// Indicator LED
+
+	//---------------------------------*
+	
+	// Setup Core 0 finished, allow Core 1 to start
+	DEBUG_DEBUG("Setup Core 0 finished");
+	rp2040.restartCore1();
+	
+}
+
+// SETUP - CORE 1:
+void setup1() {
+
+	delay(1000); // Give Core 0 a head start
 
 	//Stepper Setup
 	//---------------------------------
 		// Pin Setup
-		pinMode(ENABLE, OUTPUT);
+	pinMode(ENABLE, OUTPUT);
 
-		// Driver Setup
-		SERIAL_PORT_2.begin(115200);
+	// Driver Setup
+	SERIAL_PORT_2.begin(115200);
 
-		driver_1.begin();					  // Start driver
-		driver_1.toff(4);					  // Not used, but required to enable the motor
-		driver_1.blank_time(24);              // Recommended blank time
-		driver_1.I_scale_analog(false);       // false = extrenal current sense resistors
-		driver_1.internal_Rsense(false);      // false = deactivates internal resistor (it can't handle necessary currents).
-		driver_1.mstep_reg_select(true);      // Microstep through URART, not by Pins.
-		driver_1.rms_current(CURRENT);        // Sets the current in milliamps.
-		driver_1.SGTHRS(STALL_VALUE);         // Set the stall value from 0-255. Higher value will make it stall quicker.
-		driver_1.microsteps(MIRCO_STEPS);     // Set microsteps.
-		driver_1.TCOOLTHRS(TCOOLS);           // Min. speed for stall detection.
-		driver_1.TPWMTHRS(0);                 // Disable StealthChop PWM.
-		driver_1.semin(0);                    // Turn off CoolStep.
-		driver_1.en_spreadCycle(false);       // Turn off SpreadCycle
-		driver_1.pdn_disable(true);           // Enable UART
 
-		// Motor Setup
-		//stepper_0.connectToPins(0, 0, 0, 0);
-		stepper_1.connectToPins(STEP_1, DIR_1, LIMIT_1, DIAG_1);
-		stepper_1.setSpeedInStepsPerSecond(SPEED);
-		stepper_1.setAccelerationInStepsPerSecondPerSecond(ACCEL);
+	// Setup Pumps
+	Pump_0.SetupPump(CURRENT, STALL_VALUE, MIRCO_STEPS, TCOOLS, STEP_0, DIR_0, LIMIT_0, DIAG_0, SPEED, ACCEL, PUMP_MAX_RANGE);
 
-		//Start Driver and Motor
-		digitalWrite(ENABLE, LOW);			// Enable motor
+	//Start Pump 
+	digitalWrite(ENABLE, LOW);			  // Enable Driver
+	
+	//---------------------------------*
 
+	// Setup finished
+	DEBUG_DEBUG("Setup Core 1 finished");
+}
+
+
+
+// END OF SETUP++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// MAIN PROGRAM (LOOP):
+	// CORE 0
+	void loop() {
+		// [...]
+		
+		/* FIFO 0 TEST - DELETE LATER
+		FIFO_R_c0 = rp2040.fifo.pop();
+		DEBUG_VERBOSE("FIFO_0: %d ", FIFO_R_c0);
+		FIFO_c0++;
+		*/
+	}
+
+	// CORE 1
+	void loop1() {
+
+		// Main Pump Functions
+		switch(Mode_c1){
+			case 0: // IDLE
+				// [...]
+
+				// DELETE LATER! DEBUG/TESTING
+				while (1) {
+					//delay(100);
+					Pump_0.Test(true);
+					//delay(100);
+					Pump_0.Test(false);
+				}
+				digitalWrite(ENABLE, HIGH);			  // Enable Driver
+
+
+				//TODO: Function, power of motors, etc.
+				//TODO: Other idle functions, such as communication and reading sensors if needed.
+				break;
+			case 1: // FEED
+				// [...]
+				//TODO: Function, Feeding (Prime, Approx., Accurate, Empty)
+				break;
+			case 2: // CALIBRATE
+				// [...]
+				//TODO: Function, Calibrate Scales
+				//TODO: Function, Calibrate Slider [?]
+				break;
+			case 3: // EMGY
+				// [...]
+				//TODO: Function, Emergency Mode
+				break;
+			default:
+				// [...]
+				// TODO: Function, Error handling
+				break;
+		}
+		
+		//---------------------------------*
+
+		/* FIFO 1 TEST - DELETE LATER
+		rp2040.fifo.push(FIFO_c1);
+		FIFO_c1++;
+		//delay(10);
+		*/
+	}
+
+// END OF MAIN PROGRAM+++++++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Code Dump / Templates - DELTE:
 		/*
 		// DELETE - TESTING ONLY: Move Home
 		bool home_test = false;
 		byte home_result = 0;
 		byte error_result = 0;
 		while (1) {
-			home_result = stepper_1.moveToHome(-1, SPEED, MAX_RANGE, START_DIST_S, true);
+			home_result = stepper_1.moveToHome(-1, SPEED, MAX_RANGE, true);
 
 			if (home_result > 0) {
 				if (home_result >= 2) {
 					DEBUG_VERBOSE("Error occured");
-					if ((stepper_1.ErrorHandling(1, home_result, SPEED, MAX_RANGE) == 0) {
+					if ((stepper_1.ErrorHandling(1, home_result, SPEED, MAX_RANGE) == 0)) {
 						//HOME AGAIN
 					}
 					else {
@@ -190,31 +293,7 @@ void setup() {
 				}
 
 				DEBUG_VERBOSE("error_result: %d", error_result);
-				DEBUG_VERBOSE("Home again in 2s..");
-				delay(2000);
-				while (1) {
-					home_result = stepper_1.moveToHome(-1, SPEED, MAX_RANGE, START_DIST_S, true);
-					if (home_result > 0) {
-					break;
-					}
-				}
 			break;
 			}
 		} DEBUG_VERBOSE("home_result: %d", home_result);
 		*/
-
-	//---------------------------------*
-}
-// END OF SETUP++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// MAIN PROGRAM (LOOP):
-void loop() {
-
-
-
-
-
-}
-// END OF MAIN PROGRAM+++++++++++++++++++++++++++++++++++++++
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
